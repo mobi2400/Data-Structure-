2ff178cc5389ce289ae45710b62e76555a7b2897
mobasshir <mobasshirkhan9931@gmail.com>
2025-09-22 19:21:51 +0530

    basic recusrion

---
+++ 01_Recursion/01_Basic.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+void fun1(int n){
+    if(n>0){
+        printf("%d\n",n);
+        fun1(n-1);
+
+    }
+}
+void fun2(int n){
+    if(n>0){
+         fun2(n-1);
+         printf("%d\n",n-1);
+    }
+}
+int main(){
+    int x = 3;
+    fun1(x);
+    fun2(x);
+    
+    return 0;
+}
