f727aea8fd89bc5457fbbf34d8f7f67d4ea1f108
mobasshir <mobasshirkhan9931@gmail.com>
2025-09-24 10:11:55 +0530

    solved problem

---
+++ A2Z/Step1/lect4/Divisor.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int Divisor(int n){
+    vector<int>v;
+    int div = n;
+    while(div>0){
+        if(n%div==0){
+            v.emplace_back(div);
+        }
+        div--;
+    }
+    sort(v.begin(),v.end());
+    for(int i=0;i<v.size();i++){
+        cout<<v[i]<<" ";
+    }
+
+}
+
+int main(){
+    
+    return 0;
+}
---
+++ A2Z/Step1/lect4/Prime.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int Prime(int n){
+    // this is the most efficient way
+    // better time complexity and space complexity
+   if(n<=1){
+    return false;
+   }
+   /*
+   while checking for prime number or divisor iterate tille sqrt(n) and not n
+   */
+   for(int i=2;i<=sqrt(n);i++){
+    if(n%i==0){
+        return false;
+    }
+   }
+   return true;
+}
+
+int main(){
+    
+    return 0;
+}
